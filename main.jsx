6d0943de00bca0a68a816852df9224b84ed4d3f5
vikabaranecz-ui <vikabaranecz@gmail.com>
2026-03-25 16:26:18 +0100

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -471,4 +471,9 @@ export default function App() {
     <Foot setPage={sP} />
     <Float />
   </div>;
+import { createRoot } from 'react-dom/client';
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
 }
